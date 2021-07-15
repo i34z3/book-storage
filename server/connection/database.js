7ad3a739a2ae8d810ee95c53c1f9dcdb2703accd
i34z3 <64761267+i34z3@users.noreply.github.com>
2021-07-15 03:58:12 -0300

    Update database.js

--- server/connection/database.js
+++ server/connection/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 require('dotenv/config')
 
-const db = mongoose.connect("mongodb+srv://developer:OIQmc0rnKKbdiThA@cluster0.8ombf.mongodb.net/bookStorage?retryWrites=true&w=majority", {  
+const db = mongoose.connect(process.env.DB_CONNECTION, {  
     useNewUrlParser: true, 
     useUnifiedTopology: true, 
     useCreateIndex: true 
